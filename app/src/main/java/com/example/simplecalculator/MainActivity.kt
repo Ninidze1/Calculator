78a96faa91e15f4800a279ff3aa00384a18a543a
Ninidze1 <giorgi.ninidze.1.@btu.edu.ge>
2020-11-09 16:42:39 +0400

    first commit

---
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.simplecalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import java.math.RoundingMode
+import java.text.DecimalFormat
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.example.simplecalculator.SecondActivity
+import java.lang.Math.round
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var resultTextView: TextView
+    private var operand: Double = 0.0
+    private var operation: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        resultTextView = findViewById(R.id.resultTextView)
+
+        findViewById<TextView>(R.id.toSecondCalc).setOnClickListener {
+            startActivity(Intent(this,SecondActivity::class.java))
+        }
+
+    }
+
+    fun numberClick(view: View) {
+
+        if (view is TextView) {
+
+            val number: String = view.text.toString()
+            var result: String = resultTextView.text.toString()
+
+            if (result == "0") {
+                result = ""
+            }
+
+            resultTextView.text = result + number
+
+        }
+
+    }
+
+    fun operationClick(view: View) {
+
+        if (view is TextView) {
+            if (resultTextView.text.isNotEmpty()) {
+                operand = resultTextView.text.toString().toDouble()
+            }
+
+            this.operation = view.text.toString()
+            resultTextView.text = ""
+
+        }
+
+    }
+
+    fun equalsClick(view: View) {
+
+        var secondOperandText: String = resultTextView.text.toString()
+        var secondOperand: Double = 0.0
+
+        if (secondOperandText.isNotEmpty()) {
+            secondOperand = secondOperandText.toDouble()
+        }
+
+        if ((operand / secondOperand) % 1.0 == 0.0) {
+            when (operation) {
+                "/" -> resultTextView.text = (operand / secondOperand).toInt().toString()
+            }
+
+        } else {
+            when (operation) {
+            "/" -> resultTextView.text = (operand / secondOperand).toString()
+        }
+
+        }
+
+        if ((operand * secondOperand) % 1.0 == 0.0) {
+            when (operation) {
+                "x" -> resultTextView.text = (operand * secondOperand).toInt().toString()
+            }
+        } else {
+            when (operation) {
+                "x" -> resultTextView.text = (operand * secondOperand).toString()
+            }
+
+        }
+
+        if ((operand + secondOperand) % 1.0 == 0.0) {
+            when (operation) {
+                "+" -> resultTextView.text = (operand + secondOperand).toInt().toString()
+            }
+        } else {
+            when (operation) {
+                "+" -> resultTextView.text = (operand + secondOperand).toString()
+            }
+
+        }
+
+        if ((operand - secondOperand) % 1.0 == 0.0) {
+            when (operation) {
+                "-" -> resultTextView.text = (operand - secondOperand).toInt().toString()
+            }
+        } else {
+            when (operation) {
+                "-" -> resultTextView.text = (operand - secondOperand).toString()
+            }
+
+        }
+
+        if (operand % 1.0 != 0.0 && secondOperand % 1.0 != 0.0) {
+
+            val number = resultTextView
+            val df = DecimalFormat("#.#############")
+            df.roundingMode = RoundingMode.CEILING
+            resultTextView.text = df.format((operand / secondOperand))
+
+        }
+
+        if (operand % 1.0 != 0.0 && secondOperand % 1.0 != 0.0) {
+
+            val number = resultTextView
+            val df = DecimalFormat("#.#############")
+            df.roundingMode = RoundingMode.CEILING
+            resultTextView.text = df.format((operand * secondOperand))
+
+        }
+
+    }
+
+    fun ACClick(view: View) {
+
+        resultTextView.text = ""
+
+    }
+
+    fun BackClick(view: View) {
+
+        val str: String = resultTextView.text.toString()
+        if (str.isNotEmpty()) {
+            resultTextView.text = str.substring(0, str.length - 1)
+
+        }
+
+    }
+
+    fun dotClick(view: View) {
+
+        if (view is TextView) {
+
+            val number: String = view.text.toString()
+            var result: String = resultTextView.text.toString()
+
+            if (result == ".") {
+                result = ""
+            }
+
+            resultTextView.text = result + number
+
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/simplecalculator/SecondActivity.kt
@@ -0,0 +1,71 @@
+
+package com.example.simplecalculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.simplecalculator.R
+import kotlinx.android.synthetic.main.activity_second.*
+import net.objecthunter.exp4j.ExpressionBuilder
+import java.lang.Exception
+import kotlin.Result
+
+class SecondActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        findViewById<TextView>(R.id.num0).setOnClickListener { setTextField("0") }
+        findViewById<TextView>(R.id.num1).setOnClickListener { setTextField("1") }
+        findViewById<TextView>(R.id.num2).setOnClickListener { setTextField("2") }
+        findViewById<TextView>(R.id.num3).setOnClickListener { setTextField("3") }
+        findViewById<TextView>(R.id.num4).setOnClickListener { setTextField("4") }
+        findViewById<TextView>(R.id.num5).setOnClickListener { setTextField("5") }
+        findViewById<TextView>(R.id.num6).setOnClickListener { setTextField("6") }
+        findViewById<TextView>(R.id.num7).setOnClickListener { setTextField("7") }
+        findViewById<TextView>(R.id.num8).setOnClickListener { setTextField("8") }
+        findViewById<TextView>(R.id.num9).setOnClickListener { setTextField("9") }
+        findViewById<TextView>(R.id.dot).setOnClickListener { setTextField(".") }
+        findViewById<TextView>(R.id.gamokleba).setOnClickListener { setTextField("-") }
+        findViewById<TextView>(R.id.sum).setOnClickListener { setTextField("+") }
+        findViewById<TextView>(R.id.division).setOnClickListener { setTextField("/") }
+        findViewById<TextView>(R.id.multiply).setOnClickListener { setTextField("*") }
+        findViewById<TextView>(R.id.leftFrchxili).setOnClickListener { setTextField("(") }
+        findViewById<TextView>(R.id.RightFrchxili).setOnClickListener { setTextField(")") }
+
+        findViewById<TextView>(R.id.AC).setOnClickListener {
+            MathOperation.text = ""
+            ResultView.text = ""
+        }
+
+        findViewById<TextView>(R.id.Back).setOnClickListener {
+            val str = MathOperation.text.toString()
+            if (str.isNotEmpty()) {
+                MathOperation.text = str.substring(0, str.length - 1)
+            }
+        }
+
+        findViewById<TextView>(R.id.equalButton).setOnClickListener {
+
+            val express = ExpressionBuilder(MathOperation.text.toString()).build()
+            val res = express.evaluate()
+
+
+            val longResult = res.toLong()
+            if (res == longResult.toDouble()) {
+                ResultView.text = longResult.toString()
+            } else {
+                ResultView.text = res.toString()
+
+            }
+        }
+    }
+
+    fun setTextField(str: String) {
+        MathOperation.append(str)
+
+    }
+}
